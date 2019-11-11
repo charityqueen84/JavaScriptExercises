c9fbccf786d81f15a1b4d6118b0702c0b20c06ef
charityqueen84 <46950834+charityqueen84@users.noreply.github.com>
2019-11-10 18:09:30 -0600

    Add files via upload

--- Practice Problems/sumNumbers.js
+++ Practice Problems/sumNumbers.js
@@ -1,6 +1,6 @@
 /*
-Exercise 1: Summation
-Create a function that takes 2 parameters, ‘num1’ and ‘num2’. Function that returns
+
+Create a function that takes 2 parameters, ‘num1’ and ‘num2’ and that returns
 the sum of all whole numbers between num1 and num2 inclusive.
 (4, 10); // returns ‘49’
 (10, 4); // returns ‘0’
