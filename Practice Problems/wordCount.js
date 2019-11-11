5328b56fad5731b0aeeb84086a22cee8fcbe6835
charityqueen84 <46950834+charityqueen84@users.noreply.github.com>
2019-11-10 20:35:27 -0600

    Add files via upload

---
+++ Practice Problems/wordCount.js
@@ -0,0 +1,30 @@
+/*Exercise 5: Word Count
+Create a function ‘wordCount’ that takes a string parameter named ‘sentence’. The function should return 
+the count of words in the ‘sentence’ parameter string. A word is considered any series of characters 
+(letter, number, punctuation) separated by one or more spaces.
+Example:
+wordCount(‘This is a short sentence!’);  // returns 5
+wordCount(‘ThisIsA!$ReallyLongWord’); // returns 1
+wordCount(‘     ‘); // returns 0 */
+
+//The filter() method creates an array filled with all array elements that pass a test (provided as a function).
+function wordCount(sentence) {
+    var words = sentence.split(' '); //saving the whole string into separate words with a comma in between each word
+    //to take out extra spaces
+    var next = words.filter(function(noSpace)  { //Filtering out anything that is a blank space. This could also be written as words.filter(noSpace => {
+        return noSpace !== ''; //It is an array containing all the array elements that pass the test. 
+        //If no elements pass the test, it returns an empty array. If all elements pass the test, the array is returned with the elements.
+    });   
+    return next.length; //Returning next.length here instead of words.length. 
+ 
+   
+}
+console.log(wordCount('This is a short sentence!'));
+
+//This works, but counts empty spaces as words. Above, using filter to filter out empty spaces.
+//function wordCount(sentence) {
+    //var words = sentence.split(' ');
+    //return words.length;  //return words also shows the elements separated by a comma
+
+//}
+//console.log(wordCount('This is a short number!));
