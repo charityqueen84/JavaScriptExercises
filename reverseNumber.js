bebc82403b0e97384234d574e277a252f81a7e3c
charityqueen84 <46950834+charityqueen84@users.noreply.github.com>
2019-11-12 10:49:52 -0600

    Update reverseNumber.js

--- reverseNumber.js
+++ reverseNumber.js
@@ -1,5 +1,17 @@
 function reverse(number) {
+  number = number + ''; // changes it to a string
+  var revNum  =  number.split('').reverse().join('');
+  return revNum;
+}
+console.log(reverse(866521)); //so I can put a number in here 
+
+
+//first solution, but I realized I had  to put a number in as a string at the bottom, so above is the 
+//solution to change it to a string in the code.
+function reverse(number) {
   var revNum  =  number.split('').reverse().join('');
   return revNum;
 }
 console.log(reverse('1235'));
+
+
