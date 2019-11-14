c206107900084705e6c68ffb729b72e467d5d1f3
charityqueen84 <46950834+charityqueen84@users.noreply.github.com>
2019-11-14 14:45:41 -0600

    Update vowelCount.js

--- vowelCount.js
+++ vowelCount.js
@@ -1,5 +1,13 @@
 //count number of vowels within a string  - couldn't get rid of error in js bin 
 
+/*The indexOf() method returns the index within the calling String object of the first occurrence of the specified value, starting the search at fromIndex. Returns -1 if the value is not found.
+Syntax -> str.indexOf(searchValue[, fromIndex])
+Parameters :
+searchValue : A string representing the value to search for.
+fromIndex(Optional)-> The index at which to start the searching forwards in the string. It can be any integer.
+
+The character(s) of the string will be counted as vowel if the condition (vowel_list.indexOf(str1[x]) !== -1) matched. */
+
 function vowels(input) {
   var vow = 'aeiouAEIOU'
   var vowSum=0
