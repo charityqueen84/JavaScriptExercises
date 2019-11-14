6100dcf94cad21ac177dfef43031fd47b7101093
charityqueen84 <46950834+charityqueen84@users.noreply.github.com>
2019-11-14 14:54:36 -0600

    Update vowelCount.js

--- vowelCount.js
+++ vowelCount.js
@@ -1,6 +1,7 @@
 //count number of vowels within a string  - couldn't get rid of error in js bin 
 
-/*The indexOf() method returns the index within the calling String object of the first occurrence of the specified value, starting the search at fromIndex. Returns -1 if the value is not found.
+/*The indexOf() method returns the index within the calling String object of the first occurrence of the specified value, 
+starting the search at fromIndex. Returns -1 if the value is not found.
 Syntax -> str.indexOf(searchValue[, fromIndex])
 Parameters :
 searchValue : A string representing the value to search for.
@@ -10,12 +11,12 @@ The character(s) of the string will be counted as vowel if the condition (vowel_
 
 function vowels(input) {
   var vow = 'aeiouAEIOU'
-  var vowSum=0
-  for (var i=0; i <=input.length; i++) {
-    if (vow.indexOf(input[i]) !==-1) {
-      vowSum +=1
+  var vowSum=0 //initializing the sum
+  for (var i=0; i <=input.length; i++) { //looping over the string and as long as it's less than the length of the input, keep going
+    if (vow.indexOf(input[i]) !==-1) { //indexOf() returns the index within the string (i within in input)
+      vowSum +=1 //adding up the number of vowels
     }
-    return vowSum
+    return vowSum //returning the total number of vowels after adding up
   }
 }
-console.log(vowels('The quick brown fox'))
+console.log(vowels('The quick brown fox')) //Method call 
